121dafdf375d3c01e92a25b448a923882ff3d758
Benjamin <91425886+moodhood@users.noreply.github.com>
2024-05-30 18:30:31 +0200

    Add files via upload

---
+++ BinairySearch(Iterative).py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        l,r = 0, len(nums)-1
+        while l <= r: 
+            mid = int(l+r)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target: 
+                l = mid+1 
+            else:
+                r = mid-1 
+        return -1
+    
+so = Solution()
+print(so.search([-1,0,3,5,9,12], 2))
