1b0066f7dcbfdb57484c3f4ea28539104085d850
Benjamin <91425886+moodhood@users.noreply.github.com>
2024-06-03 23:56:46 +0200

    Add files via upload

---
+++ SearchA2DMatrix(BinairySearch).py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        Rows, Collumn = len(matrix), len(matrix[0])
+        Top, Bottom = 0, Rows-1
+
+        while Top <= Bottom:
+            mid = (Top+Bottom)//2
+            if target > matrix[mid][-1]:
+                Top = Rows + 1 
+            elif target < matrix[mid][0]:
+                Bottom = Rows - 1
+            else:
+                break
+        
+
+        if not (Top <= Bottom):
+            return False
+        row = (Top+Bottom)//2
+        l, r = 0, Collumn-1
+
+        while l <= r:
+            mid = (l+r)//2
+            if target > matrix[row][mid]:
+                l = mid + 1 
+            elif target > matrix[row][mid]:
+                r = mid - 1 
+            else: 
+                return True
+        return False 
